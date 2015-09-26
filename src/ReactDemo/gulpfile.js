f40c4aa1630f08c2309726967ab47b3ef7592382
matt welson <me_number3@hotmail.com>
2015-09-26 15:15:27 +1200

    Set up gulp to build css

--- src/ReactDemo/gulpfile.js
+++ src/ReactDemo/gulpfile.js
@@ -4,7 +4,29 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
 */
 
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var uglify = require('gulp-uglify');
+var rimraf = require('rimraf');
 
-gulp.task('default', function () {
-    // place code for your default task here
+gulp.task('sass', ['clean:css'], function () {
+    return gulp.src('Content/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('wwwroot/css'));
 });
+
+gulp.task('clean:css', function (cb) {
+    return rimraf('wwwroot/css/*.min.css', cb);
+});
+
+gulp.task('min:css', [ 'sass' ], function () {
+    return gulp.src(["wwwroot/css/*.css", "!wwwwroot/css/*.min.css"])
+        .pipe(concat('wwwroot/css/main.min.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest('.'));
+});
+
+gulp.task('css:watch', function () {
+    gulp.watch('Content/scss/*.scss', ['sass', 'clean:css', 'min:css']);
+})
